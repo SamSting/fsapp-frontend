ed64b76afb530b0a7d77a852ff6724ba1f4aacbd
SamSting <101314920+SamSting@users.noreply.github.com>
2023-07-25 14:36:35 +0530

    Add files via upload

---
+++ src/utils/HandleApi.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const baseUrl = "https://smeapp.onrender.com";
+
+const getAllToDo = (setToDo) => {
+  axios.get(baseUrl)
+    .then(({ data }) => {
+      console.log('data ---> ', data);
+      setToDo(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching ToDo data:', error);
+      
+    });
+};
+
+const addToDo = (text, setText, setToDo) => {
+
+    axios
+        .post(`${baseUrl}/save`, { text })
+        .then((data) => {
+            console.log(data);
+            setText("")
+            getAllToDo(setToDo)
+        })
+		.catch((err) => console.log(err))
+
+
+}
+
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+
+    axios
+        .post(`${baseUrl}/update`, { _id: toDoId, text })
+        .then((data) => {
+            setText("")
+            setIsUpdating(false)
+            getAllToDo(setToDo)
+        })
+		.catch((err) => console.log(err))}
+
+
+
+const deleteToDo = (_id, setToDo) => {
+
+			axios
+				.post(`${baseUrl}/delete`, { _id })
+				.then((data) => {
+					console.log(data)
+					getAllToDo(setToDo)
+				})
+				.catch((err) => console.log(err))
+		
+		}
+
+
+export { getAllToDo,addToDo,updateToDo,deleteToDo};
